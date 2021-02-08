828a9e5e0b018929d67b5be2d5e23ed41cd90162
caizhuoyao <70246542+caizhuoyao@users.noreply.github.com>
2021-02-08 19:04:20 +0800

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+<div id="app">
+    <example></example>
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
